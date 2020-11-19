7855b160148b2cab1bb33d7b7c9de80ee7d2e67c
Lyudmyla Boiko <itsforyou.com.ua@gmail.com>
2020-11-19 10:59:43 +0200

    second task

--- second-task.js
+++ second-task.js
@@ -7,12 +7,22 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
+
+//   callback(updatedUsers);
+// };
+
 const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map(user =>
+  return new Promise((resolve, reject) => {
+     const updatedUsers = allUsers.map(user =>
     user.name === userName ? { ...user, active: !user.active } : user,
   );
-
-  callback(updatedUsers);
+    resolve(updatedUsers);
+  })
+  
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
@@ -20,8 +30,8 @@ const logger = updatedUsers => console.table(updatedUsers);
 /*
  * Сейчас работает так
  */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
 
 /*
  * Должно работать так
